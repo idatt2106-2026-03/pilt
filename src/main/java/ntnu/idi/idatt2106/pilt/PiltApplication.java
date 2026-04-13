a98eb7f155a41a5de1a4e544112f996b2f95058c
Alexander Owren Elton <alexander.owren.elton@hotmail.com>
2026-04-13 13:20:54 +0200

    init pilt commit

---
+++ src/main/java/ntnu/idi/idatt2106/pilt/PiltApplication.java
@@ -0,0 +1,13 @@
+package ntnu.idi.idatt2106.pilt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiltApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ntnu/idi/idatt2106/pilt/PiltApplicationTests.java
@@ -0,0 +1,13 @@
+package ntnu.idi.idatt2106.pilt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
