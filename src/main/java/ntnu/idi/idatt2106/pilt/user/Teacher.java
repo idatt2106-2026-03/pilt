2f1f69afa93b6651fe5f9bf99e96c981fde5d083
Oleandertengesdal <oleander.tengesdal@icloud.com>
2026-04-14 09:00:41 +0200

    feat/user/implentation(): updated teacher class

--- src/main/java/ntnu/idi/idatt2106/pilt/user/Teacher.java
+++ src/main/java/ntnu/idi/idatt2106/pilt/user/Teacher.java
@@ -1,8 +1,10 @@
 package ntnu.idi.idatt2106.pilt.user;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
 
 /**
  * Class representing a teacher in the system. This class extends the User class and adds specific attributes and relationships for teachers.
@@ -10,7 +12,15 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "teachers")
 public class Teacher extends User {
+
+    @Email
+    @Column(nullable = false, unique = true)
     private String schoolEmail;
+
+    @Override
+    public Role getRole() {
+        return Role.TEACHER;
+    }
 
     //@OneToMany(mappedBy = "teacher")
     //private List<Classroom> classrooms;
