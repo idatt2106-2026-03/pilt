00903dc67b9ef0d5cd3bbd8319dd05b597340fab
torfoz <tord.fosse@gmail.com>
2026-04-14 11:20:16 +0200

    feat(task): created taskprogress

---
+++ src/main/java/ntnu/idi/idatt2106/pilt/features/stoppingplace/model/TaskProgress.java
@@ -0,0 +1,56 @@
+package ntnu.idi.idatt2106.pilt.features.stoppingplace.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Track a players progress of a task.
+ **/
+@Entity
+public class TaskProgress {
+  /**
+   * Unique identifier for the task.
+   */
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  private Player player;
+
+  @ManyToOne
+  private Task task;
+
+  /**
+   * Whether the player has completed, or started the task.
+   */
+  @Getter
+  @Setter
+  private TaskStatus taskStatus;
+
+  /**
+   * Star rating (0–3).
+   */
+  @Getter
+  @Setter
+  private int stars;
+
+  /**
+   * Track progress steps.
+   **/
+  @Getter
+  private int progress;
+
+  /**
+   * Time of task completion.
+   **/
+  @Getter
+  private LocalDateTime completedA;
+}
