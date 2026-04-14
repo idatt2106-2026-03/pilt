98e2fb17fe7d75cfa942e3e06b0a7a78b52eb2c5
Oleandertengesdal <oleander.tengesdal@icloud.com>
2026-04-14 09:00:31 +0200

    feat/user/implentation(): updated student class

--- src/main/java/ntnu/idi/idatt2106/pilt/user/Student.java
+++ src/main/java/ntnu/idi/idatt2106/pilt/user/Student.java
@@ -1,15 +1,28 @@
 package ntnu.idi.idatt2106.pilt.user;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "students")
 public class Student extends User {
+
     private int level;
+
     private int totalScore;
+
+    @Column(nullable = false)
+    @NotBlank
+    private String feideUsername;
+
+    @Column(nullable = false)
+    @NotBlank
+    private String displayName;
+
+    @Override
+    public Role getRole() {
+        return Role.STUDENT;
+    }
 
     //@ManyToOne
     //private Classroom classroom;
