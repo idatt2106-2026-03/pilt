3b9a98c56cbb13fb439164beec31291e8cc431a4
Oleandertengesdal <oleander.tengesdal@icloud.com>
2026-04-14 08:40:15 +0200

    feat/user/implentation(): created and implemented Role Enum

---
+++ src/main/java/ntnu/idi/idatt2106/pilt/user/Role.java
@@ -0,0 +1,10 @@
+package ntnu.idi.idatt2106.pilt.user;
+
+/**
+ * Enum representing the different roles a user can have in the system.
+ * There are two roles: TEACHER and STUDENT.
+ */
+public enum Role {
+    TEACHER,
+    STUDENT
+}
--- src/main/java/ntnu/idi/idatt2106/pilt/user/User.java
+++ src/main/java/ntnu/idi/idatt2106/pilt/user/User.java
@@ -1,15 +1,19 @@
-package ntnu.idi.idatt.2106.pilt.user;
+package ntnu.idi.idatt2106.pilt.user;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
+/**
+ * Abstract class representing a user in the system. This class is extended by both Teacher and Student classes.
+ */
 @Entity
 @Getter
 @Setter
 @Table(name = "users")
 @Inheritance(strategy = InheritanceType.JOINED)
-public interface User {
+public abstract class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,4 +34,7 @@ public interface User {
     @Column(nullable = false)
     @NotBlank
     private String password;
+
+    @NotBlank
+    private Role role;
 }
